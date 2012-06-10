1c9662903896d417cecb199c11f923089a7f4e34
patrick <patrick.j.bybee@gmail.com>
2012-06-09 18:43:16 -0700

    Catch errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,11 @@ class ApplicationController < ActionController::Base
 
   def current_user
     if session[:user_id]
-      @current_user = User.find(session[:user_id])
+      begin
+        @current_user = User.find(session[:user_id])
+      rescue
+        session[:user_id] = nil
+      end
     end unless @current_user
 
     @current_user     
